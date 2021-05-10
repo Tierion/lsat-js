fe18bc137858dac2b71ace929cfcf58487c48ac0
Buck Perley <bucko.perley@gmail.com>
2021-05-09 19:20:59 -0500

    add npm prepare script

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './identifier';
-export * from './caveat';
-export * from './lsat';
-export * from './types';
-export { expirationSatisfier } from './satisfiers';
--- dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lsat';
-export * from './satisfier';
