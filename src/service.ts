3f2c52234306fdc1ad78eeac51e404d92f077611
Buck Perley <bucko.perley@gmail.com>
2022-02-07 19:25:07 -0600

    add capability encoding

--- src/service.ts
+++ src/service.ts
@@ -1,27 +1,28 @@
-/** 
+/**
  * @file Services are a special kind of caveat based off of
- * the official lsat spec by 
+ * the official lsat spec by
  * [Lightning Labs](https://lsat.tech/macaroons#target-services).
  * These have certain expectations around value encoding and also support
- * tiers of capabilities where services have service level capabilities and 
+ * tiers of capabilities where services have service level capabilities and
  * capabilities in turn can have constraints.
  * See below for an example from lightning loop.
- * 
+ *
  * @example
  *  services = lightning_loop:0
  *  lightning_loop_capabilities = loop_out,loop_in
  *  loop_out_monthly_volume_sats = 200000000
- * 
+ *
  */
 
 import bufio from 'bufio'
+import { Caveat } from './caveat'
 
 export class NoServicesError extends Error {
   constructor(...params: string[]) {
     super(...params)
     this.name = 'NoServicesError'
     this.message = 'no services found'
-        // Maintains proper stack trace for where our error was thrown (only available on V8)
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, NoServicesError)
     }
@@ -29,12 +30,24 @@ export class NoServicesError extends Error {
 }
 
 export class InvalidServicesError extends Error {
-  constructor(message?:string) {
+  constructor(message?: string) {
     super(message)
     this.name = 'InvalidServicesError'
+    if (!message) this.message = 'service must be of the form "name:tier"'
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, InvalidServicesError)
+    }
+  }
+}
+
+export class InvalidCapabilitiesError extends Error {
+  constructor(message?: string) {
+    super(message)
+    this.name = 'InvalidCapabilitiesError'
     if (!message)
-      this.message = 'service must be of the form "name:tier"'
-        // Maintains proper stack trace for where our error was thrown (only available on V8)
+      this.message = 'capabilities must be a string or array of strings'
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, InvalidServicesError)
     }
@@ -58,38 +71,44 @@ export class Service extends bufio.Struct {
 }
 
 /**
- * 
+ *
  * @param {string} s - raw services string of format `name:tier,name:tier`
  * @returns array of Service objects or throws an error
  */
-export const decodeServicesCaveat = (s:string): Service[] | Error => {
+export const decodeServicesCaveat = (s: string): Service[] | Error => {
   if (!s.length) throw new NoServicesError()
 
   const services: Service[] = []
   const rawServices = s.split(',')
 
-  for(const serviceString of rawServices) {
+  for (const serviceString of rawServices) {
     const [service, tier] = serviceString.split(':')
     // validation
     if (!service || !tier) throw new InvalidServicesError()
     if (isNaN(+tier)) throw new InvalidServicesError('tier must be a number')
-    if (!isNaN(+service)) throw new InvalidServicesError('service name must be a string')
-    services.push(new Service({name: service, tier: +tier}))
+    if (!isNaN(+service))
+      throw new InvalidServicesError('service name must be a string')
+    services.push(new Service({ name: service, tier: +tier }))
   }
 
   return services
 }
 
-export const encodeServicesCaveatValue = (services: Service[]): string | Error => {
+export const encodeServicesCaveatValue = (
+  services: Service[]
+): string | Error => {
   if (!services.length) throw new NoServicesError()
 
   let rawServices = ''
 
-  for (let i=0; i < services.length; i++) {
-    const service = services[i];
-    if (!(service instanceof Service)) throw new InvalidServicesError('not a valid service')
-    if (!service.name) throw new InvalidServicesError('service must nave a name') 
-    if (service.tier !== 0 && !service.tier) throw new InvalidServicesError('service must have a tier')
+  for (let i = 0; i < services.length; i++) {
+    const service = services[i]
+    if (!(service instanceof Service))
+      throw new InvalidServicesError('not a valid service')
+    if (!service.name)
+      throw new InvalidServicesError('service must nave a name')
+    if (service.tier !== 0 && !service.tier)
+      throw new InvalidServicesError('service must have a tier')
 
     rawServices = rawServices.concat(`${service.name}:${service.tier}`)
     // add a comma at the end if it's not the same
@@ -98,8 +117,26 @@ export const encodeServicesCaveatValue = (services: Service[]): string | Error =
   return rawServices
 }
 
+export const SERVICE_CAPABILITIES_SUFFIX = '_capabilities'
 
-/**
- * services=meme:0
- * meme_capabilities=
- */
+export const createNewCapabilitiesCaveat = (
+  serviceName: string,
+  _capabilities?: string | string[]
+): Caveat => {
+  let capabilities: string
+  if (!_capabilities) {
+    capabilities = ''
+  } else if (Array.isArray(_capabilities)) {
+    capabilities = _capabilities.join(',')
+  } else if (typeof _capabilities !== 'string') {
+    throw new InvalidCapabilitiesError()
+  } else {
+    capabilities = _capabilities
+  }
+
+  return new Caveat({
+    condition: serviceName + SERVICE_CAPABILITIES_SUFFIX,
+    value: capabilities,
+    comp: '=',
+  })
+}
--- tests/service.spec.ts
+++ tests/service.spec.ts
@@ -1,5 +1,13 @@
-import {expect} from 'chai'
-import {NoServicesError, InvalidServicesError, decodeServicesCaveat, Service, encodeServicesCaveatValue} from '../src/service'
+import { expect } from 'chai'
+import {
+  NoServicesError,
+  InvalidServicesError,
+  decodeServicesCaveat,
+  encodeServicesCaveatValue,
+  InvalidCapabilitiesError,
+  createNewCapabilitiesCaveat,
+  SERVICE_CAPABILITIES_SUFFIX,
+} from '../src/service'
 
 describe.only('services', () => {
   it('can encode and decode service caveats', () => {
@@ -61,12 +69,58 @@ describe.only('services', () => {
         ).to.throw(t.err)
         continue
       }
-      
+
       const services = decodeServicesCaveat(t.value)
       // check to make typescript happy
       if (!(services instanceof Error)) {
         const rawServices = encodeServicesCaveatValue(services)
         expect(rawServices).to.equal(t.value)
+      }
+    }
+  })
+
+  it('can create new capabilities caveat', () => {
+    const tests = [
+      {
+        name: 'valid string args',
+        serviceName: 'foo',
+        capabilities: 'bar,baz',
+      },
+      {
+        name: 'valid array args',
+        serviceName: 'foo',
+        capabilities: ['bar', 'baz'],
+      },
+      {
+        name: 'supports empty  capabilities',
+        serviceName: 'foo',
+        capabilities: '',
+      },
+      {
+        name: 'invalid capabilities',
+        serviceName: 'foo',
+        capabilities: 12,
+        err: InvalidCapabilitiesError,
+      },
+    ]
+
+    for (const t of tests) {
+      if (t.err) {
+        expect(
+          // @ts-expect-error
+          () => createNewCapabilitiesCaveat(t.serviceName, t.capabilities),
+          `expected "${t.name}" to throw`
+        ).to.throw(InvalidCapabilitiesError)
+        continue
+      }
+      const caveat = createNewCapabilitiesCaveat(t.serviceName, t.capabilities)
+      expect(caveat.condition).to.equal(
+        t.serviceName + SERVICE_CAPABILITIES_SUFFIX
+      )
+      if (Array.isArray(t.capabilities)) {
+        expect(caveat.value).to.equal(t.capabilities.join(','))
+      } else {
+        expect(caveat.value).to.equal(t.capabilities)
       }
     }
   })
